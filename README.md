d540137db3b06a1a6df74ca7cd2277120d7655e7
Joe Burgess <joe@flatironschool.com>
2014-05-19 11:21:17 -0400

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+---
+languages: javascript
+tags: beginner, integers
+---
+
+# Is Between
+
+Your goal is to write a function called `is_between`. It does exactly you think. The function should take three arguments: `num`, `min`, and `max`. The function should `true` or `false` if num is in between min and max inclusive.
---
+++ spec/is_between.spec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('is_between', function() {
+
+  var is_between;
+  beforeEach(function() {
+    is_between = require('../lib/is_between');
+  });
+
+  describe( "#is_between", function() {
+    it("should return true if the number is between the two numbers", function() {
+        expect(is_between(15, 12, 18)).toBe(true);
+      });      
+  
+    it("should return false if the number is not between the two numbers", function() {
+        expect(is_between(5, 12, 18)).toBe(false);
+      });   
+  }); 
+});
